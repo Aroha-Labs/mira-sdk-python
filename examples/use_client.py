0f57e8d74ebf7aa3d98db00763d8c74b5bf46463
Sanchay <sanchay.pahalwani@gmail.com>
2024-09-05 03:45:28 +0530

    Init commit for integrating prompt manager

--- examples/use_client.py
+++ examples/use_client.py
@@ -6,5 +6,5 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.mira.client.mira_client import MiraClient
 
 client = MiraClient("my_flow.yaml")
-result = client.execute({"topic": "Artificial Intelligence"})
+result = client.execute({"name": "Lionel Messi"})
 print(result)
