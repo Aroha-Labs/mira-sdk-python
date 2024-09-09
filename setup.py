f9f4b777bcf31d81c1dcae0ac4e90006f5abb17e
sarim2000 <sarimbleedblue@gmail.com>
2024-09-09 13:53:13 +0530

    feat: add setup.py for mira_sdk
    - Added setup.py for packaging the mira_sdk Python SDK
    - Includes package name, version, description, long description, URL, and classifiers

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mira_sdk",
+    version="0.0.1",
+    description="A Python SDK for the Mira API",
+    long_description=open("README.md").read(),
+    url="https://github.com/Aroha-Labs/mira-sdk-python",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
