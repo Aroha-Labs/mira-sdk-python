18f10c32ddfb02268c09f4424e32b2180c6ee354
Sanchay <sanchay.pahalwani@gmail.com>
2024-09-10 18:45:11 +0530

    Integrated LiteLLM, added templates for sdk and cli, fixed import src bug

--- examples/generate_story.py
+++ examples/generate_story.py
@@ -1,12 +1,14 @@
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from src.mira import Flow
 
 
 # Initialize the flow from the YAML file
-flow = Flow('example_flow.yaml')
+flow = Flow('src/templates/person.yaml')
 
 # Define the input for our story generation
-input_text = {"topic": "Cristiano Ronaldo"}
+input_text = {"coin": "ZRX", "date": "Ice age"}
 
 # Execute the flow
 result = flow.execute(input_text)
